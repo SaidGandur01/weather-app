bd2eab29ae62b0c16270e2e184a819347c8965e6
Said Gandur <ing.saidgandur@gmail.com>
2023-11-24 16:34:48 -0500

    Updating angular and adding spinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { HomePageComponent } from './home/home-page.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { WeatherPageComponent } from './weather-page/weather-page.component';
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgxLoadingModule, ngxLoadingAnimationTypes } from 'ngx-loading';
 
 @NgModule({
   declarations: [
@@ -17,8 +17,15 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
   ],
   imports: [
     AppRoutingModule,
-    BrowserAnimationsModule,
     BrowserModule,
+    NgxLoadingModule.forRoot({
+      animationType: ngxLoadingAnimationTypes.wanderingCubes,
+      backdropBackgroundColour: "rgba(0,0,0,0.1)",
+      backdropBorderRadius: "4px",
+      primaryColour: "#181f29",
+      secondaryColour: "#181f29",
+      tertiaryColour: "#181f29",
+    }),
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
--- src/app/home/home-page.component.ts
+++ src/app/home/home-page.component.ts
@@ -8,6 +8,7 @@ import { Router } from '@angular/router';
 })
 export class HomePageComponent implements OnInit {
 
+  loading = false;
   constructor(private router: Router) { }
 
   ngOnInit(): void {
