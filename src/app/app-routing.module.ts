e98b17ac7ebfcc069259c9c74db220ea3ae51d77
Said Gandur <ing.saidgandur@gmail.com>
2023-11-24 16:05:11 -0500

    Adding routting strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home/home-page.component';
+import { WeatherPageComponent } from './weather-page/weather-page.component';
 
 const routes: Routes = [
   { path: "home", component: HomePageComponent },
+  { path: 'weather/:id', component: WeatherPageComponent },
   { path: '', redirectTo: "home", pathMatch: 'full' },
 ];
 
--- src/app/weather-page/weather-page.component.ts
+++ src/app/weather-page/weather-page.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-weather-page',
   templateUrl: './weather-page.component.html',
   styleUrls: ['./weather-page.component.scss']
 })
-export class WeatherPageComponent implements OnInit {
+export class WeatherPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  subscription: Subscription[] = [];
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    this.subscription.push(
+      this.route.paramMap.subscribe((params: ParamMap) => {
+        const id = params.get('id');
+        if (id) {
+          this.getWeatherInformation(id);
+        }
+      })
+    )
+  }
+
+  getWeatherInformation(id: string): void {
+    console.log('this is the id from path url in weather page: ', id)
+  }
+
+  goToHomePage(): void {
+    this.router.navigate(['home']);
+  }
+
+  ngOnDestroy(): void {
+      
   }
 
 }
