3ef9b413e6afe76c2732e616543fb6b5dbb7154c
Said Gandur <ing.saidgandur@gmail.com>
2023-11-24 17:04:48 -0500

    Updating navbar component;

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TItems, TWeatherCode } from 'src/utils/types';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  
+  currentSelection: TItems = ''
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onItemSelected(selection: TItems, code: TWeatherCode): void {
+    this.currentSelection = selection;
+    this.router.navigate(['weather', code]);
+  }
+
+  goToHomePage(): void {
+    this.router.navigate(['home']);
   }
 
 }
