9376f9452268f3a0c9467f34f649baaa02960ce8
Said Gandur <ing.saidgandur@gmail.com>
2023-11-26 13:40:13 -0500

    Adding cahrtjks library in project

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { TWeatherCode } from 'src/utils/types';
 import { Observable } from 'rxjs';
+import { IWeatherResponse } from 'src/utils/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,7 @@ export class WeatherService {
 
   constructor(private httpClient: HttpClient) { }
 
-  getWeatherDetails(districtCode: TWeatherCode): Observable<any> {
+  getWeatherDetails(districtCode: TWeatherCode): Observable<IWeatherResponse> {
     const fullUrl = `${this.baseUrl}/${districtCode}/31,80/forecast`;
     return this.httpClient.get<any>(fullUrl)
   }
--- src/app/weather-page/weather-page.component.ts
+++ src/app/weather-page/weather-page.component.ts
@@ -1,8 +1,18 @@
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { Subscription } from 'rxjs';
 import { WeatherService } from '../services/weather.service';
 import { TWeatherCode } from 'src/utils/types';
+import { Chart, registerables } from 'chart.js';
+import { IWeatherResponse } from 'src/utils/interfaces';
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-weather-page',
@@ -10,9 +20,18 @@ import { TWeatherCode } from 'src/utils/types';
   styleUrls: ['./weather-page.component.scss'],
 })
 export class WeatherPageComponent implements OnInit, OnDestroy {
-
   loading = true;
   subscription: Subscription[] = [];
+
+  @Input()
+  height = 200;
+
+  weatherInformation!: IWeatherResponse;
+
+  @ViewChild('lineChart', { static: false, read: ElementRef })
+  lineChartReference!: ElementRef;
+
+  lineChartInstance!: Chart;
 
   constructor(
     private route: ActivatedRoute,
@@ -35,11 +54,54 @@ export class WeatherPageComponent implements OnInit, OnDestroy {
     this.subscription.push(
       this.weatherService
         .getWeatherDetails(districtCode)
-        .subscribe((weatherResponse) => {
-          this.loading = false
-          console.log('this is the weather response: ', weatherResponse);
+        .subscribe((weatherResponse: IWeatherResponse) => {
+          this.loading = false;
+          this.weatherInformation = weatherResponse;
+          setTimeout(() => {
+            this.buildChart();
+          }, 500);
         })
     );
+  }
+
+  private buildChart(): void {
+    console.log('this is the weather response: ', this.weatherInformation);
+
+    const labels = ['Jan', 'Feb', 'Mar', 'May', 'Jun', 'Jul', 'Aug'];
+    const data = {
+      labels: labels,
+      datasets: [
+        {
+          label: 'My First Dataset',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          fill: false,
+          borderColor: 'rgb(75, 192, 192)',
+          tension: 0.1,
+        },
+      ],
+    };
+
+    const config = {
+      type: 'line',
+      data: data,
+    };
+
+    this.renderChart(config);
+  }
+
+  private renderChart(data: any): void {
+    console.log('aca: ', this.lineChartReference);
+    if (this.lineChartReference) {
+      const ctx = this.lineChartReference.nativeElement.getContext('2d');
+      this.destroyChart();
+      this.lineChartInstance = new Chart(ctx, data);
+    }
+  }
+
+  private destroyChart(): void {
+    if (this.lineChartInstance) {
+      this.lineChartInstance.destroy();
+    }
   }
 
   goToHomePage(): void {
@@ -50,5 +112,6 @@ export class WeatherPageComponent implements OnInit, OnDestroy {
     this.subscription.map((subscription: Subscription) => {
       subscription.unsubscribe();
     });
+    this.destroyChart();
   }
 }
